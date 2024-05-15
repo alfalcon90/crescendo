ffa0a0d6926cfbe0f0a9f5c30a13d7eab36c7fc2
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2024-05-15 15:15:55 -0700

    Add Link component

---
+++ src/components/Link.tsx
@@ -0,0 +1,27 @@
+import { Url } from "next/dist/shared/lib/router/router";
+import NextLink from "next/link";
+
+export default function Link({
+  href,
+  dark,
+  underline,
+  children,
+}: {
+  href: Url;
+  dark?: boolean;
+  underline?: boolean;
+  children: React.ReactNode;
+}) {
+  return (
+    <NextLink
+      className={`${
+        dark ? "text-beige" : "text-green-dark"
+      } ${
+        underline ? "underline" : ""
+      } focus:ring-green rounded-sm underline-offset-8 hover:underline px-1 uppercase tracking-wider focus:outline-none focus:ring`}
+      href={href}
+    >
+      {children}
+    </NextLink>
+  );
+}
