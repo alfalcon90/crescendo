8fb913c1a8d87003e764611c84232988876e7ff8
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2024-05-16 15:21:46 -0700

    Tweak SectionContainer width

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -7,7 +7,7 @@ export default function SectionContainer({
 }) {
   return (
     <div
-      className={`${className} flex w-full max-w-6xl flex-col items-center space-y-4 px-4 py-12 sm:space-y-8 sm:py-24`}
+      className={`${className} flex w-full max-w-8xl flex-col items-center space-y-4 px-4 py-12 sm:space-y-8 sm:py-24`}
     >
       {children}
     </div>
