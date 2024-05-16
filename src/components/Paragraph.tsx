460e3720c9abd8ac9a0c26e20092e89ec8a93b5d
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2024-05-16 10:59:52 -0700

    Add text components

---
+++ src/components/Paragraph.tsx
@@ -0,0 +1,21 @@
+export default function Paragraph({
+  children,
+  className,
+  large,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  large?: boolean;
+}) {
+  return (
+    <p
+      className={`${
+        large
+          ? "text-2xl leading-relaxed"
+          : "text-base font-medium text-green-dark"
+      } ${className}`}
+    >
+      {children}
+    </p>
+  );
+}
