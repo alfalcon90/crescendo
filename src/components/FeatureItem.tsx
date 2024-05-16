3327f5c99611dff7f2d8757e2ca25e3024c4ce3a
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2024-05-16 15:21:33 -0700

    Add FeatureItem

---
+++ src/components/FeatureItem.tsx
@@ -0,0 +1,49 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import Paragraph from "./Paragraph";
+import Heading3 from "./Heading3";
+
+export default function FeatureItem({
+  src,
+  alt,
+  title,
+  body,
+  reverse,
+  className,
+}: {
+  src: string | StaticImport;
+  alt: string;
+  title: string;
+  body?: string;
+  dark?: boolean;
+  reverse?: boolean;
+  className?: string;
+}) {
+  return (
+    <div
+      className={`flex w-full justify-center ${className}`}
+    >
+      <div
+        className={`flex w-full max-w-7xl flex-col ${
+          reverse ? "lg:flex-row-reverse" : "lg:flex-row"
+        } items-center justify-between sm:space-x-8`}
+      >
+        <div className="max-w-[420px] md:max-w-[520px] lg:max-w-none">
+          <Image
+            className="scale-[1.1]"
+            src={src}
+            alt={alt}
+            width={1000}
+            height={1000}
+          />
+        </div>
+        <div className="flex flex-col space-y-8 w-full lg:w-1/2">
+          <div className="flex flex-col space-y-4  ">
+            <Heading3>{title}</Heading3>
+            <Paragraph large>{body}</Paragraph>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
