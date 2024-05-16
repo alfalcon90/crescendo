ba78d016ed795a59fc371d2248947d4dd3c0b196
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2024-05-16 11:01:03 -0700

    Add Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,68 @@
+import { Url } from "next/dist/shared/lib/router/router";
+import Link from "next/link";
+import React, { FunctionComponentElement, MouseEventHandler } from "react";
+import { IconBaseProps, IconType } from "react-icons";
+
+export default function Button({
+  leadingIcon,
+  trailingIcon,
+  href,
+  onClick,
+  children,
+  className,
+  type,
+  dark,
+  disabled,
+}: {
+  leadingIcon?: IconType;
+  trailingIcon?: IconType;
+  href?: Url;
+  onClick?: MouseEventHandler;
+  children: string;
+  className?: string;
+  type?: "submit" | "reset" | "button";
+  dark?: boolean;
+  disabled?: boolean;
+}) {
+  if (type != undefined) {
+    return (
+      <button
+        className={_className(dark, className)}
+        onClick={onClick}
+        type={type}
+        aria-disabled={disabled}
+        disabled={disabled}
+      >
+        {_leadingIcon(leadingIcon)}
+        {children}
+        {_trailingIcon(trailingIcon)}
+      </button>
+    );
+  } else {
+    return (
+      <Link
+        className={_className(dark, className)}
+        href={href ?? {}}
+        type={type}
+        aria-disabled={disabled}
+      >
+        {_leadingIcon(leadingIcon)}
+        {children}
+        {_trailingIcon(trailingIcon)}
+      </Link>
+    );
+  }
+}
+
+const _className = (dark?: boolean, className?: string ): string =>
+  `${dark ? 'text-beige border border-beige hover:bg-beige/25' : 'text-green-dark border border-red hover:bg-beige-dark'}   focus:ring-green flex items-center rounded-full px-5 py-3 uppercase tracking-wider focus:outline-none focus:ring ${className}`;
+
+const _leadingIcon = (
+  icon?: IconType,
+): FunctionComponentElement<IconBaseProps> | undefined =>
+  icon && React.createElement(icon, { className: "mr-2 h-[24px] w-[24px]" });
+
+const _trailingIcon = (
+  icon?: IconType,
+): FunctionComponentElement<IconBaseProps> | undefined =>
+  icon && React.createElement(icon, { className: "ml-2 h-[24px] w-[24px]" });
