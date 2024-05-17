221bbb5ffc05ca2ce53fb6ab52ecfbc2264d84a0
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2024-05-17 16:20:13 -0700

    Make contact responsive

--- src/app/contact/ContactSection.tsx
+++ src/app/contact/ContactSection.tsx
@@ -6,12 +6,12 @@ import Link from "next/link";
 
 export default function ContactSection() {
   return (
-    <SectionContainer className="[&]:flex-row max-w-6xl space-x-16">
-      <div className="flex w-full flex-col space-y-8">
-        <SectionHeader className="[&]:text-start lg:[&]:items-start" innerClassName="lg:[&]:items-start [&]:space-y-1" title="Get in touch"><p className="text-red" >You can reach me anytime via <Link className="underline" href="mailto:shafer.jason@gmail.com">shafer.jason@gmail.com</Link></p></SectionHeader>
+    <SectionContainer className="md:[&]:flex-row max-w-6xl space-x-8">
+      <div className="min-w-[400px] flex w-full flex-col space-y-8">
+        <SectionHeader className="[&]:text-start [&]:items-start" innerClassName="[&]:items-start [&]:space-y-1" title="Get in touch"><p className="text-red" >You can reach me anytime via <Link className="underline" href="mailto:shafer.jason@gmail.com">shafer.jason@gmail.com</Link></p></SectionHeader>
         <ContactForm />
       </div>
-      <Image src="/illy-mail.png" alt="" height={500} width={500} />
+      <Image className="hidden md:inline w-full" src="/illy-mail.png" alt="" height={200} width={200} />
     </SectionContainer>
   );
 }
