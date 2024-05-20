0b2bed1486c531a673e348ca0a836bcbdc81ed9c
shaferjason <shafer.jason@gmail.com>
2024-05-20 15:40:17 -0600

    Removed email address

--- src/app/contact/ContactSection.tsx
+++ src/app/contact/ContactSection.tsx
@@ -8,7 +8,7 @@ export default function ContactSection() {
   return (
     <SectionContainer className="md:[&]:flex-row max-w-6xl space-x-8">
       <div className="min-w-[400px] flex w-full flex-col space-y-8">
-        <SectionHeader className="[&]:text-start [&]:items-start" innerClassName="[&]:items-start [&]:space-y-1" title="Get in touch"><p className="text-red" >You can reach me anytime via <Link className="underline" href="mailto:shafer.jason@gmail.com">shafer.jason@gmail.com</Link></p></SectionHeader>
+        <SectionHeader className="[&]:text-start [&]:items-start" innerClassName="[&]:items-start [&]:space-y-1" title="Get in touch"><p className="text-red" >I look forward to hearing from you!</p></SectionHeader>
         <ContactForm />
       </div>
       <Image className="hidden md:inline w-full" src="/illy-mail.png" alt="" height={200} width={200} />
