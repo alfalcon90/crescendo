d04d8bc01cdb5f1d4d9f60b5d68b2f22bcdef89e
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2024-05-20 21:12:11 -0700

    Clean up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
       <div className="flex justify-center w-full h-[1000px] sm:h-[1200px] md:h-[800px] lg:h-[1504px] bg-[url('/hero-mask.svg')] bg-center">
         <div className="flex flex-col-reverse items-center sm:items-start md:flex-row w-full h-min max-w-6xl px-4 pt-10 md:pt-[100px] md:space-x-8">
           <div className="flex flex-col space-y-6 pt-8 sm:pt-0 md:pt-8 sm:px-8 md:px-0 md:space-y-10">
-            <div className="">
+            <div>
               <Heading1 className="text-center sm:text-start [&]:text-beige">Crescendo</Heading1>
               <Paragraph large className="text-center sm:text-start [&]:text-beige">Performance Consulting</Paragraph>
             </div>
