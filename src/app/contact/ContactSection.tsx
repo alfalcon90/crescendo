82b8f557776d8592598e69478098c271243ad8ec
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2024-05-20 21:02:23 -0700

    Fix contact form bug

--- src/app/contact/ContactSection.tsx
+++ src/app/contact/ContactSection.tsx
@@ -2,12 +2,11 @@ import Image from "next/image";
 import ContactForm from "./ContactForm";
 import SectionContainer from "@/components/SectionContainer";
 import SectionHeader from "@/components/SectionHeader";
-import Link from "next/link";
 
 export default function ContactSection() {
   return (
     <SectionContainer className="md:[&]:flex-row max-w-6xl space-x-8">
-      <div className="min-w-[400px] flex w-full flex-col space-y-8">
+      <div className="md:min-w-[400px] flex w-full flex-col space-y-8">
         <SectionHeader className="[&]:text-start [&]:items-start" innerClassName="[&]:items-start [&]:space-y-1" title="Get in touch"><p className="text-red" >I look forward to hearing from you!</p></SectionHeader>
         <ContactForm />
       </div>
